acabf388c641d103bb1ea1cd8a4f6c8fcee99b39
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2024-04-10 16:42:14 -0300

    add functions to add and remove tasks

--- src/components/Cont.jsx
+++ src/components/Cont.jsx
@@ -3,24 +3,59 @@ import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 //import Box from '@mui/material/Box';
 //import FormControl from '@mui/material/FormControl';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export default function Cont () {
-    const [seleccion, setSeleccion] = React.useState('');
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  const [newTask, setNewTask] = useState("")
+
+    /*const [seleccion, setSeleccion] = React.useState('');
 
     const handleChange = (event) => {
       setSeleccion(event.target.value);
-    };
+    };*/
+
+    function addTasks() {
+      setTasks([...tasks, {id: crypto.randomUUID(), task: newTask}]);
+      localStorage.setItem(
+        "tasks", 
+        JSON.stringify([
+          ...tasks,
+           {id: crypto.randomUUID(), task: newTask},
+        ])
+      )
+    }
+
+    function removeTasks(idTask) {
+      setTasks(tasks.filter((task) => task.id !== idTask ))
+      localStorage.setItem(
+        "tasks", 
+        JSON.stringify([
+          ...tasks,
+        ])
+      )
+    }
     return (
         <Container sx={{ backgroundColor: "#880e4f", height: "100vh", width: "100vh", padding: "16px" }}>
-      <TextField fullWidth id="outlined-basic" label="Tarea" variant="outlined" sx={{margin: "10px"}} />
+      <TextField fullWidth id="outlined-basic" placeholder="Ingrese una tarea" type="text" variant="outlined" sx={{margin: "10px"}} onChange={(e) =>setNewTask(e.target.value)} />
+      <Button variant="contained"sx={{ backgroundColor: "#880e4f", width: "20vh", padding: "16px", margin: "20px"}} onClick={addTasks} >Send</Button>
+     <Container> 
+      {tasks.map((task) => (
+        <Typography variant="h6" key={task.id}>{task.task}<Button onClick={() => removeTasks(task.id)}>X</Button><Button >seleccionar</Button></Typography>
+        ))}
+        </Container>
       {/* <Box sx={{ minWidth: 120 }}>*/}
       {/* <FormControl fullWidth >*/}
       
-        <InputLabel id="demo-simple-select-label">Seleccionar</InputLabel>
+      {/*<InputLabel id="demo-simple-select-label">Seleccionar</InputLabel>
         <Select fullWidth  sx={{ minWidth: 120, margin: "10px" }}
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -31,11 +66,11 @@ export default function Cont () {
           <MenuItem value={1}>Todas</MenuItem>
           <MenuItem value={2}>Completas</MenuItem>
           <MenuItem value={3}>Incompletas</MenuItem>
-        </Select>
+    </Select>*/}
      {/*</FormControl> */} 
     {/* </Box>*/}
     
-      <Button variant="contained"sx={{ backgroundColor: "#880e4f", width: "20vh", padding: "16px", margin: "20px"}} >Send</Button>
+      
     
         </Container>
       
